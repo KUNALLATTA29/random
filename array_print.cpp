0b3d7450707a00fd4e9df0667a3239871dd6084f
KUNALLATTA29 <95677039+KUNALLATTA29@users.noreply.github.com>
2024-04-19 23:12:19 +0530

    Add files via upload

---
+++ array_print.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int myArray[] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; ++i) {
+        cout << myArray[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
