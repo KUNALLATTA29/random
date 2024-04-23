4f2bc2e2acd49cef42554f958d38855bb4304ec9
KUNALLATTA29 <95677039+KUNALLATTA29@users.noreply.github.com>
2024-04-23 23:47:50 +0530

    Add files via upload

---
+++ ternary.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int a =  5;
+    int b = 3;
+    string ans = a>b ? "a is great" : "b is great";
+    cout<< ans;
+    return 0;
+}
